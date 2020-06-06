73156524d06af50b2867908ef7fbf669030fc1b7
Sidd <iProdigy@users.noreply.github.com>
2020-06-06 16:13:31 -0500

    Deprecate old java time in favor of Instant

--- api/src/main/java/com/github/philippheuer/events4j/api/domain/IEvent.java
+++ api/src/main/java/com/github/philippheuer/events4j/api/domain/IEvent.java
@@ -2,13 +2,27 @@ package com.github.philippheuer.events4j.api.domain;
 
 import com.github.philippheuer.events4j.api.service.IServiceMediator;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public interface IEvent {
 
+    @Deprecated
     void setFiredAt(Calendar calendar);
 
+    default void setInstant(Instant instant) {
+        setFiredAt(GregorianCalendar.from(ZonedDateTime.ofInstant(instant, ZoneId.systemDefault())));
+    }
+
+    @Deprecated
     Calendar getFiredAt();
+
+    default Instant getInstant() {
+        return getFiredAt().toInstant();
+    }
 
     void setEventId(String id);
 
