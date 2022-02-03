1662bd2f3613df030ffd1f10811e851de2e2ee43
Thomas 'SubSide' van den Bulk <t@vdbulk.net>
2022-02-03 23:52:15 +0100

    feat: add kotlin module (#69)
    * Feat: Kotlin extension functions
    
    * fix: MR feedback

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,27 @@
+// In this section you declare the dependencies for your production and test code
+dependencies {
+    // Project
+    api(project(":api"))
+    api(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.6.0")
+
+    // Testing
+    testImplementation(group = "org.jetbrains.kotlinx", name="kotlinx-coroutines-test", version = "1.6.0")
+    testImplementation(project(":core"))
+    testImplementation(project(":handler-simple"))
+    testImplementation(testFixtures(project(":core")))
+}
+
+tasks.test {
+    useJUnitPlatform {
+        includeTags("unittest")
+        excludeTags("integration")
+    }
+}
+
+publishing.publications.withType<MavenPublication> {
+    artifactId = "events4j-kotlin"
+    pom {
+        name.set("Events4J - Kotlin")
+        description.set("Events4J - Kotlin extension functions")
+    }
+}
