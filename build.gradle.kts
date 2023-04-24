5cb452d9d9cd45513543f9664d741a8a63e0f7e2
Philipp Heuer <git@philippheuer.me>
2023-04-24 19:23:54 +0200

    chore(deps): update lombok to v1.18.26

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
         language.set(me.philippheuer.projectcfg.domain.ProjectLanguage.JAVA)
         type.set(me.philippheuer.projectcfg.domain.ProjectType.LIBRARY)
         javaVersion.set(JavaVersion.VERSION_1_8)
-        lombokVersion.set("1.18.24")
+        lombokVersion.set("1.18.26")
         artifactGroupId.set("com.github.philippheuer.events4j")
 
         pom = { pom ->
